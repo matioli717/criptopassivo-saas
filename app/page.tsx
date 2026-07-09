a80f7549f7f26646035bb544a57db650d1cb3496
matioli717 <gabehabbo4@gmail.com>
2026-07-09 04:54:03 +0000

    ajustes de dashboard, IR por mês e CSS

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,18 @@
 import { redirect } from "next/navigation";
 import { createClient } from "@/lib/supabase/server";
 
-export default async function Home() {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: { code?: string };
+}) {
   const supabase = createClient();
+
+  if (searchParams.code) {
+    await supabase.auth.exchangeCodeForSession(searchParams.code);
+    redirect("/dashboard");
+  }
+
   const {
     data: { user },
   } = await supabase.auth.getUser();
